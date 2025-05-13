94cd684cb209361909f91047f9532e4219c17422
Alexi Dg <alexisdurangomez588@gmail.com>
2025-05-13 12:49:57 -0500

    "fix: :sparkles: add create user"

--- dia_6/HomeWork.py
+++ dia_6/HomeWork.py
@@ -4,69 +4,6 @@
 # Diccionarios
 #Un DICccionario  es una colección de elementos , donde cada elementos insertado tiene una llave úntica, 
 # la cual va acompañada de un valor 
-'''
-Pepito es Cool
-{
-    "nombre":"Pedro",
-    "apellido":"Gómez"
-}
-'''
-miPrimerDiccionario={
-    "nombre":"Pedro",
-    "apellido":"Gómez",
-    "edad":25
-}
-print(miPrimerDiccionario)
-print(type(miPrimerDiccionario))
-
-##Para recorrer un DICcionario debes llamar a la llave
-print(miPrimerDiccionario["nombre"])
-print(type(miPrimerDiccionario["nombre"]))
-# Se puede recorrer con puntos en algunos casos
-'''
-print(miPrimerDiccionario.nombre)
-'''
-##Para reemplazar un valor del diccionario
-miPrimerDiccionario["nombre"]="Pedro"
-nombre = miPrimerDiccionario["nombre"]
-apellido= miPrimerDiccionario["apellido"]
-print(miPrimerDiccionario["nombre"]+" "+miPrimerDiccionario["apellido"])
-
-miPrimerDiccionario["ciudadNacimiento"]="Monteria"
-
-print(miPrimerDiccionario)
-miPrimerDiccionario["ciudadNacimiento"]="Bucaramanga"
-
-print(miPrimerDiccionario)
-#Un objeto tiene varios atributos, 
-# que pertenecen al mismo objeto. 
-listaPersonas=[]
-listaPersonas.append(miPrimerDiccionario)
-print("")
-print("")
-print(listaPersonas)
-listaPersonas.append({
-    "nombre":"Corpus",
-    "apellido":"Bejarano",
-    "edad":27
-})
-print("")
-print("")
-print(listaPersonas)
-print("")
-print("")
-print(listaPersonas[1])
-print(type(listaPersonas[1]))
-print(listaPersonas[0]["edad"])
-
-#Recorrer listas con diccionarios
-for i in range(len(listaPersonas)):
-    print("#################")
-    print("####Persona #",i+1," ####")
-    print("#################")
-    print("Nombre:",listaPersonas[i]["nombre"])
-    print("Apllido:",listaPersonas[i]["apellido"])
-    print("Edad:",listaPersonas[i]["edad"])
 
 ##Diccionario con listas
 diccionarioRobusto={
@@ -74,8 +11,18 @@ diccionarioRobusto={
     "nombre":"Pedro",
     "apellido":"Gómez",
     "edad":25,
-    "telefonos":[{"codigo":57,"numero":3023019865,"tipo":"trabajo"}
-                 ,{"codigo":1,"numero":3983054625,"tipo":"personal"}]
+    "telefonos":[
+        {
+            "codigo":57,
+            "numero":3023019865,
+            "tipo":"trabajo"
+        },
+        {
+            "codigo":1,
+            "numero":3983054625,
+            "tipo":"personal"
+        }
+    ]
 }
 diccionarioRobusto2={
     "id":2,
@@ -88,21 +35,24 @@ diccionarioRobusto2={
 listaRobusta=[]
 listaRobusta.append(diccionarioRobusto)
 listaRobusta.append(diccionarioRobusto2)
-print("")
-print("")
-print(listaRobusta)
-print("")
-print("")
+
+
+# print("")# 
+# print("")
+# print(listaRobusta)
+# print("")
+# print("")
 #print(listaRobusta[0]["telefonos"][1]['numero'])
 
-for i in range(len(listaRobusta[0]["telefonos"])):
-    if(listaRobusta[0]["telefonos"][i]['tipo']=="trabajo"):
-        print(listaRobusta[0]["telefonos"][i]['numero'])
-print("")
-print("")
-numeroPrimeraPersona=listaRobusta[0]["telefonos"][1]['numero']
-tipoNumeroPP=listaRobusta[0]["telefonos"][1]['tipo']
-print(str(numeroPrimeraPersona)+ tipoNumeroPP)
+# for i in range(len(listaRobusta[0]["telefonos"])):
+    # if(listaRobusta[0]["telefonos"][i]['tipo']=="trabajo"):
+        # print(listaRobusta[0]["telefonos"][i]['numero'])
+# print("")
+# print("")
+# numeroPrimeraPersona=listaRobusta[0]["telefonos"][1]['numero']
+# tipoNumeroPP=listaRobusta[0]["telefonos"][1]['tipo']
+# print(str(numeroPrimeraPersona)+ tipoNumeroPP)
+
 
 booleanito = True
 while(booleanito):
@@ -117,11 +67,52 @@ while(booleanito):
     print("5. Eliminar a una persona en específico")
     print("6. Cerrar programa")
     opcionUsuario=int(input("Escoja una opción (Numérica):"))
+
     if(opcionUsuario==1):
         print("#################")
         print("#### Crear Persona ####")
         print("#################")
         diccionarioVacio={}
+        nombre = input('DIga el nombre: ')
+        apellido = input('DIga el apellido: ')
+        edad = input('DIga el edad: ')
+        catPhone = int(input('Diga la cantidad de telefonos: '))
+
+        diccionarioUsuario = {
+            "id": 0,
+            "nombre":nombre,
+            "apellido":apellido,
+            "edad":edad,
+            "telefonos":[] 
+        }
+        
+
+        for j in range(catPhone):
+            code = int(input('Código: '))
+            phone = int(input('Número: '))
+            work = input('Tipo: ')
+
+            # Variable para crear el diccionario de la información de telefotno
+            data = {
+                "codigo":code,
+                "numero":phone,
+                "tipo":work
+            }
+            diccionarioUsuario['telefonos'].append(data) # Aquí
+        
+        counter = 0
+        id_user = 0
+        # Ciclo para encontrar recorrer la lista de usuariso ya creados
+        for i in range(len(listaRobusta)):
+            counter += 1
+            # Condicional para ver si coincide el id actal del iterador con uno ya existente
+            # Y se asigna el que si
+            if listaRobusta[i] == diccionarioUsuario['nombre']:
+                id_user = counter  # N sé como asignar el numero del indice, porque así asigna es el contenido
+
+        diccionarioUsuario['id'] = id_user
+        listaRobusta.append(diccionarioUsuario)
+        print(f'Persona {nombre} creada correctamente.')
 
     elif(opcionUsuario==2):
         for i in range(len(listaRobusta)):
@@ -167,7 +158,16 @@ while(booleanito):
                         print("#### - Tipo: Es su número de Trabajo")
                     
                     print("---------------------------")
-            
+
+    elif(opcionUsuario==4):
+        print("#################")
+        print("#### Actualizar a una persona en específico ####")
+        print("#################")
+
+        idPersona = int(input('Por favor, escriba el ID del usuario: '))
+
+        # for i range(len(listaRobusta)):
+            # if(listaRobusta)
             
     elif(opcionUsuario==6):
         print("Chaousssss")
