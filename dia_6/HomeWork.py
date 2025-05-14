f15885440ca288aed0405f73a9c41e05e0c0259e
Alexi Dg <alexisdurangomez588@gmail.com>
2025-05-14 00:32:14 -0500

    "fix: :sparkles: list formatting"

--- dia_6/HomeWork.py
+++ dia_6/HomeWork.py
@@ -74,16 +74,36 @@ diccionarioRobusto={
     "nombre":"Pedro",
     "apellido":"Gómez",
     "edad":25,
-    "telefonos":[{"codigo":57,"numero":3023019865,"tipo":"trabajo"}
-                 ,{"codigo":1,"numero":3983054625,"tipo":"personal"}]
+    "telefonos": [
+        {
+            "codigo":57,
+            "numero":3023019865,
+            "tipo":"trabajo"
+        },
+        {
+            "codigo":1,
+            "numero":3983054625,
+            "tipo":"personal"
+        }
+    ]
 }
 diccionarioRobusto2={
     "id":2,
     "nombre":"Corpus",
     "apellido":"Bejarano",
     "edad":27,
-    "telefonos":[{"codigo":58,"numero":2323057565,"tipo":"trabajo"}
-                 ,{"codigo":22,"numero":6857493658,"tipo":"personal"}]
+    "telefonos": [
+        {
+            "codigo":58,
+            "numero":2323057565,
+            "tipo":"trabajo"
+        },
+        {
+            "codigo":22,
+            "numero":6857493658,
+            "tipo":"personal"
+        }
+    ]
 }
 listaRobusta=[]
 listaRobusta.append(diccionarioRobusto)
